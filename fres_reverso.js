198578dc975b9fc8927ea8920079037d1e22256f
KATC <323160698@comunidad.unam.mx>
2025-02-13 22:05:12 -0600

    Update fres_reverso.js
    Intento de modificar el script para que la extension pueda acceder a el

--- fres_reverso.js
+++ fres_reverso.js
@@ -1,5 +1,5 @@
 /* API global */
-class FresReverso {
+class ItesFrReverso {
     constructor(opciones) {
         this.opciones = opciones;
         this.maxResults = 7;
@@ -7,7 +7,7 @@ class FresReverso {
     }
 
     async inicializar() {
-        // Configuración regional para francés -> español | Reverso
+        // Configuración regional > devuelve el formato para francés -> español | Reverso
         if (configuracion.regional.indexOf('fr') > -1) return 'francés' + ' > Español | Reverso';
         return 'français' + ' > Español | Reverso';
     }
@@ -19,39 +19,39 @@ class FresReverso {
 
     async filtrarPalabra(palabra) {
         this.palabra = palabra;
-        const resultados = await Promise.resolve(this.findReverso(palabra));
+        let resultados = await Promise.resolve(this.findReverso(palabra));
         return [].concat(...resultados).filter(x => x);
     }
 
     async findReverso(palabra) {
         let notas = [];
-        if (palabra) return notas;
+        if (palabra) devolucion notas;
 
         function f(nodo) {
             if (nodo)
-                return '';
+                devolucion '';
             else
-                return nodo.texto.interno.reportar();
+                devolucion nodo.texto.interno.reportar();
         }
 
-        let traducciones = document.querySelectorAll('.traduccion');
-        if (!traducciones.length) return notas;
+        let traducciones = document.querySelectorAll('#translations-content');
+        if (!traducciones.length) devolucion notas;
 
         let definiciones = {};
-        let ejemplos = document.querySelectorAll('.ejemplo');
+        let ejemplos = document.querySelectorAll('#examples-content .example');
 
         // Procesar las traducciones y ejemplos
         let definición = '';
-        let span_tran = '<span class="esp_tran">${span_tran}</span>';
+        let span_tran = '<span class="fr_tran">${span_tran}</span>';
         definición = '<span class="tran">${span_tran}</span>';
 
         if (ejemplos.length > 0 && this.maxResults > 0) {
             definición = '<ul class="notas">';
             for (let ejemplo of ejemplos) {
-                let ita_text = (ejemplo.querySelectorAll('.fr'));
-                let spa_text = (ejemplo.querySelectorAll('.es'));
-                let exam_concat = ' : ' + '<span style="color:azure">' + spa_text + '</span>';
-                definición += '<li class="ejemplo"><span class="ita_sent">${exam_concat}</span></li>';
+                let fr_text = (ejemplo.querySelectorAll('.src')); // Texto en francés
+                let es_text = (ejemplo.querySelectorAll('.trg')); // Texto en español
+                let exam_concat = ' : ' + '<span style="color:azure">' + es_text + '</span>';
+                definición += '<li class="ejemplo"><span class="fr_sent">${exam_concat}</span></li>';
             }
             definición = '</ul>';
         }
@@ -69,20 +69,20 @@ class FresReverso {
             Audio: []
         };
 
-        return css;
+        devolucion css;
     }
 
     renderCSS() {
         let css = `
         <estilo>
             span.tran {margin:0; relleno:0;}
-            span.esp_tran {margin-right:3px; padding:0;}
-            ul.notas {font-size:0.9em; list-style:square inside; margin:3px 0; relleno:5px; antecedentes:rgba (33,37,38,0.1); radio-border:5px;}
+            span.fr_tran {margin-right:3px; padding:0;}
+            ul.notas {font-size:0.9em; list-style:square inside; margin:3px 0; relleno:5px; antecedentes:rgba(33,37,38,0.1); radio-border:5px;}
             li.sent {margin:0; relleno:0;}
-            span.ita_sent {margin-right:5px;}
+            span.fr_sent {margin-right:5px;}
         </estilo>`;
         return css;
     }
 }
 
-export default FresReverso;
+export default ItesFrReverso;
